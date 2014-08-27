b5c02956174fdf175d7a674ad8c75088dafe6c8f
Jack Jamieson <jamieson.jack@gmail.com>
2014-08-27 19:07:24 -0400

    add full screen camera rotated correctly

--- src/com/natedog/spermcount/FullscreenActivity.java
+++ src/com/natedog/spermcount/FullscreenActivity.java
@@ -6,62 +6,59 @@ import android.graphics.BitmapFactory;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 public class FullscreenActivity extends Activity {
 
-	   private Camera cameraObject;
-	   private ShowCamera showCamera;
-	   private ImageView pic;
-	   public static Camera isCameraAvailiable(){
-	      Camera object = null;
-	      try {
-	         object = Camera.open(); 
-	      }
-	      catch (Exception e){
-	      }
-	      return object; 
-	   }
+	private Camera cameraObject;
+	private ShowCamera showCamera;
 
-	   private PictureCallback capturedIt = new PictureCallback() {
+	public static Camera isCameraAvailiable() {
+		Camera object = null;
+		try {
+			object = Camera.open();
+		} catch (Exception e) {
+		}
+		return object;
+	}
 
-	      @Override
-	      public void onPictureTaken(byte[] data, Camera camera) {
+	private PictureCallback capturedIt = new PictureCallback() {
 
-	      Bitmap bitmap = BitmapFactory.decodeByteArray(data , 0, data .length);
-	      if(bitmap==null){
-	         Toast.makeText(getApplicationContext(), "not taken", Toast.LENGTH_SHORT).show();
-	      }
-	      else
-	      {
-	         Toast.makeText(getApplicationContext(), "taken", Toast.LENGTH_SHORT).show();    	
-	      }
-	      cameraObject.release();
-	   }
+		@Override
+		public void onPictureTaken(byte[] data, Camera camera) {
+
+			Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+			if (bitmap == null) {
+				Toast.makeText(getApplicationContext(), "not taken",
+						Toast.LENGTH_SHORT).show();
+			} else {
+				//what to do when the user captures a picture
+				Toast.makeText(getApplicationContext(), "taken",
+						Toast.LENGTH_SHORT).show();
+			}
+			cameraObject.release();
+		}
 	};
 
-	   @Override
-	   protected void onCreate(Bundle savedInstanceState) {
-	      super.onCreate(savedInstanceState);
-	      setContentView(R.layout.activity_fullscreen);
-	      pic = (ImageView)findViewById(R.id.imageView1);
-	      cameraObject = isCameraAvailiable();
-	      showCamera = new ShowCamera(this, cameraObject);
-	      FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-	      preview.addView(showCamera);
-	   }
-	   public void snapIt(View view){
-	      cameraObject.takePicture(null, null, capturedIt);
-	   }
+	//called then the app starts
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_fullscreen);
+		cameraObject = isCameraAvailiable();
+		showCamera = new ShowCamera(this, cameraObject);
+		FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+		preview.addView(showCamera);
+	}
 
-	   /*
-	   @Override
-	   public boolean onCreateOptionsMenu(Menu menu) {
-	      getMenuInflater().inflate(R.menu.main, menu);
-	      return true;
-	   }*/
+	public void snapIt(View view) {
+		cameraObject.takePicture(null, null, capturedIt);
+	}
+
+	/*
+	 * @Override public boolean onCreateOptionsMenu(Menu menu) {
+	 * getMenuInflater().inflate(R.menu.main, menu); return true; }
+	 */
 }
--- src/com/natedog/spermcount/ShowCamera.java
+++ src/com/natedog/spermcount/ShowCamera.java
@@ -9,13 +9,16 @@ import android.view.SurfaceView;
 
 public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
 
-	   private SurfaceHolder holdMe;
-	   private Camera theCamera;
+	   private SurfaceHolder holdMe;//holds the camera preview
+	   private Camera theCamera;//reference to the camera from the main activity
 
 	   public ShowCamera(Context context,Camera camera) {
 	      super(context);
 	      theCamera = camera;
+	      theCamera.setDisplayOrientation(90);//rotate the camera 90 degrees
+	      
 	      holdMe = getHolder();
+	      holdMe.setKeepScreenOn(true);//disable the screen from sleeping
 	      holdMe.addCallback(this);
 	   }
 
@@ -34,5 +37,8 @@ public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
 
 	   @Override
 	   public void surfaceDestroyed(SurfaceHolder arg0) {
+		   //safely release the camera, IMPORTANT!
+		  // theCamera.stopPreview();
+		   //theCamera.release();
 	   }
 }
